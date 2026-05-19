559880e0987b6df9113ee48fe5cd6f869166a141
Avishka Weerasinghe <avishkaweerasinghe02@gmail.com>
2026-05-19 14:45:41 +0530

    add authentication

---
+++ backend/models/userModel.js
@@ -0,0 +1,43 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    phone: {
+      type: DataTypes.STRING,
+    },
+
+    role: {
+      type: DataTypes.ENUM("student", "organizer", "admin"),
+      defaultValue: "student",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default User;
