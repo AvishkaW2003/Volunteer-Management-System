29c2f49f11ab0860210da5555d2fdbc26d2ec70e
irosha16378-bit <irosha16378@gmail.com>
2026-06-07 18:45:48 +0530

    chore(home): import required modules, context hooks and icons

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  Calendar, Award, Star, CheckCircle, Trophy, Bell,
+  Users, HandHelping, ArrowRight, Menu, X,
+  Mail, Phone, MapPin, Facebook, Twitter, Linkedin
+} from "lucide-react";
+import { useAuth } from "../context/AuthContext";
+import "./HomePage.css";
