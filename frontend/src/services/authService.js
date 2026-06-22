01442a64a2d9181cf92f97e5b329cfa77a00877a
Keshomigha <keshomighasajeeve@gmail.com>
2026-06-22 22:42:38 +0530

    Modified authservices

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,8 +2,13 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:5000/api/auth';
 
-export const registerUser = async (userData) => {
-  const response = await axios.post(`${API_URL}/register`, userData);
+export const registerStudent = async (userData) => {
+  const response = await axios.post(`${API_URL}/register/student`, userData);
+  return response.data;
+};
+
+export const registerOrganizer = async (userData) => {
+  const response = await axios.post(`${API_URL}/register/organizer`, userData);
   return response.data;
 };
 
