ab251f6b22cec037972bca854785f654d6204e9f
Keshomigha <keshomighasajeeve@gmail.com>
2026-06-24 08:16:36 +0530

    Modified eventModel.js

--- backend/models/eventModel.js
+++ backend/models/eventModel.js
@@ -1,5 +1,5 @@
 import { DataTypes } from "sequelize";
-import sequelize from "../config/db.js";
+import sequelize from "../config/database.js";
 import User from "./userModel.js";
 
 const Event = sequelize.define("Event", {
@@ -33,46 +33,7 @@ const Event = sequelize.define("Event", {
     type: DataTypes.INTEGER,
     allowNull: false,
   },
-  status: {
-    type: DataTypes.ENUM("pending", "approved", "rejected"),
-    defaultValue: "pending",
+  approvalStatus: {
+    type: DataTypes.ENUM("Pending", "Approved", "Rejected"),
+    defaultValue: "Pending",
   },
-
-  
-  image: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    defaultValue: "https://images.unsplash.com/photo-1593113598332-cd288d649433?w=800",
-  },
-  time: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    defaultValue: "10:00 AM",
-  },
-  reputationPoints: {
-    type: DataTypes.INTEGER,
-    defaultValue: 10,
-  },
-
-  category: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-
-  skills: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-  },
-});
-
-/*
-Relationship
-One user → many events
-One event → belongs to one user
-*/
-
-User.hasMany(Event);
-
-Event.belongsTo(User);
-
-export default Event;
